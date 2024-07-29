637d90ec69db1a748b3fd053aa506fedf340ae8b
Ryan Katayi <108867595+RyanKatayi@users.noreply.github.com>
2024-07-29 22:50:18 +0200

    feat: changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,10 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { AuthProvider } from '../lib/AuthContext';
+import { AuthProvider, useAuth } from '../lib/AuthContext';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -30,29 +30,46 @@ export default function RootLayout() {
 
   return (
     <AuthProvider>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-          <Stack.Screen name="onboarding/WelcomeScreen" options={{ headerShown: false }}/>
-          <Stack.Screen name="onboarding/TrackYourGoal" options={{ headerShown: false }}/>
-          <Stack.Screen name="onboarding/GetBurn" options={{ headerShown: false }}/>
-          <Stack.Screen name="onboarding/EatWell" options={{ headerShown: false }}/>
-          <Stack.Screen name="onboarding/ImproveSleepQuality" options={{ headerShown: false }}/>
-          <Stack.Screen name="authentication/ForgotPasswordScreen" options={{ headerShown: false }} />
-          <Stack.Screen name="authentication/LoginForm" options={{ headerShown: false }} />
-          <Stack.Screen name="authentication/RegisterForm" options={{ headerShown: false }} />
-          <Stack.Screen name="authentication/SuccessScreen" options={{ headerShown: false }} />
-          <Stack.Screen name="authentication/AuthForm" options={{ headerShown: false }} />
-          <Stack.Screen name="information/ProfileCompletion" options={{ headerShown: false }}/>
-          {/* <Stack.Screen name="fitness/WorkoutsScreen" options={{ title: 'Workouts' }} />
-          <Stack.Screen name="fitness/AddWorkout" options={{ title: 'Add Workout' }} />
-          <Stack.Screen name="fitness/NutritionScreen" options={{ title: 'Nutrition' }} /> */}
-          <Stack.Screen name="fitnessScreens/AddMeal" options={{ title: 'Add Meal', headerShown: false, headerBackTitleVisible: true }} />
-          <Stack.Screen name="fitnessScreens/AddProgress" options={{ title: 'Add Progress' }} />
-          <Stack.Screen name="fitnessScreens/AddWorkout" options={{ title: 'Add Workout' }} />
-        </Stack>
-      </ThemeProvider>
+      <AuthCheck>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+            <Stack.Screen name="onboarding/WelcomeScreen" options={{ headerShown: false }}/>
+            <Stack.Screen name="onboarding/TrackYourGoal" options={{ headerShown: false }} />
+            <Stack.Screen name="onboarding/GetBurn" options={{ headerShown: false }} />
+            <Stack.Screen name="onboarding/EatWell" options={{ headerShown: false }} />
+            <Stack.Screen name="onboarding/ImproveSleepQuality" options={{ headerShown: false }} />
+            <Stack.Screen name="authentication/ForgotPasswordScreen" options={{ headerShown: false }} />
+            <Stack.Screen name="authentication/LoginForm" options={{ headerShown: false }} />
+            <Stack.Screen name="authentication/RegisterForm" options={{ headerShown: false }} />
+            <Stack.Screen name="authentication/SuccessScreen" options={{ headerShown: false }} />
+            <Stack.Screen name="authentication/AuthForm" options={{ headerShown: false }} />
+            <Stack.Screen name="information/ProfileCompletion" options={{ headerShown: false }} />
+            <Stack.Screen name="fitnessScreens/AddMeal" options={{ title: 'Add Meal', headerShown: false }} />
+            <Stack.Screen name="fitnessScreens/AddWater" options={{ title: 'Add Water', headerShown: false }} />
+            <Stack.Screen name="fitnessScreens/EditProfileScreen" options={{ title: 'Edit Profile', headerShown: false }} />
+          </Stack>
+        </ThemeProvider>
+      </AuthCheck>
     </AuthProvider>
   );
 }
+
+const AuthCheck: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    return null; 
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="authentication/AuthForm" options={{ headerShown: false }} />
+      </Stack>
+    );
+  }
+
+  return <>{children}</>;
+};
